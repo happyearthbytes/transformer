5d728ab9fff9143c2a2ddc860223c55f18b29cee
Joe DiPilato <joe.dipilato@gmail.com>
2024-03-11 22:36:25 -0400

    update tokenizer

---
+++ xformer.py
@@ -0,0 +1,391 @@
+#!/usr/bin/env python
+
+import collections
+import datasets
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+import torchtext
+from pprint import pprint
+import time
+from functools import partial
+from dataclasses import dataclass, field
+from typing import Generator
+
+from pprint import pprint, pformat
+import inspect
+from pygments import highlight
+from pygments.lexers import Python3Lexer
+from pygments.formatters import TerminalTrueColorFormatter
+import requests
+from pathlib import Path
+import spacy
+
+class Format:
+    def __init__(self, style):
+        self.style = style
+    def print_py(self, code):
+        print(highlight(code, Python3Lexer(), formatter=TerminalTrueColorFormatter(style=self.style)))
+    def print_obj(self, item):
+        self.print_py(pformat(item))
+    def print_src(self, item):
+        self.print_py(inspect.getsource(item))
+
+IDS = [13600, 19699, 19846, 27478, 27479, 27480, 30073, 30685, 30935, 30976, 31156, 31329, 31447, 31641, 31793, 31855, 31950, 32063, 32097, 32182, 32294, 32423, 32607, 32689, 32758, 32783, 32860, 32940, 32975, 33052, 33127, 33189, 33239, 33295, 33365, 33427, 33550, 33614, 33698, 33750, 34018, 34047, 34082, 34116, 34162, 34209, 34312, 34405, 34533, 34612, 34702, 34751, 34878, 34992, 35092, 35169, 35236, 35306, 35398, 35473, 35561, 35606, 35747, 35925, 36104, 36226, 36452, 36735, 37064, 37160, 37282, 37461, 37523, 37610, 37736, 37806, 37880, 37984, 38143, 38202, 38304, 38401, 38454, 38539, 38622, 38709, 38799, 38892, 38964, 39029, 39127, 39232, 39353, 39435, 39521, 39632, 39700, 39775, 39908, 40009, 40096, 40156, 40370, 40538, 40641, 40769, 40863, 40956, 41055, 41156, 41264, 41343, 41472, 41567, 41685, 41773, 41902, 42048, 42173, 42342, 42473, 42552, 42638, 42736, 42854, 43060, 43254, 43427]
+
+@dataclass
+class DownloadConfig:
+    url_fmt: str = "https://www.gutenberg.org/cache/epub/{id}/pg{id}.txt"
+    ids: list[int] = field(default_factory=lambda :IDS)
+    download_path: Path = Path("data/raw_text")
+
+    def get_items(self) -> Generator[tuple[str,str], None, None]:
+        for id in self.ids:
+            yield (f"{id}.txt", self.url_fmt.format(id=id))
+
+@dataclass
+class Config:
+    seed: int = 0 # use fixed seed for testing
+    preview: slice = field(default_factory=lambda : slice(None,10,None))  # first N items
+    example_item: int = 1337
+    max_length: int = 512 # trim sequences
+    min_freq: int = 5 # filter uncommon tokens
+    default_token: str = "<unk>"
+    special_tokens: list[str] = field(default_factory=lambda : ["<unk>", "<pad>"])
+    batch_size: int = 512
+    lr: float = 0.001
+    num_epochs: int = 20
+    heads: int = 4
+    emb: int = 32
+    device: str = "cuda"
+    start_time: float = field(default_factory=lambda : time.time())
+    last_time: float = 0.0
+    download_txt_path: Path = Path("data/raw_text")
+    formatted_txt_path: Path = Path("data/raw_text")
+    tokenized_txt_path: Path = Path("data/tokenized_text")
+
+    # eval_interval = 250 # keep frequent because we'll overfit
+    # eval_iters = 200
+    # always_save_checkpoint = False
+    # wandb_log = False # override via command line if you like
+    # wandb_project = 'shakespeare-char'
+    # wandb_run_name = 'mini-gpt'
+    # gradient_accumulation_steps = 1
+    # batch_size = 64
+    # block_size = 256 # context of up to 256 previous characters
+    # n_layer = 6
+    # n_head = 6
+    # n_embd = 384
+    # dropout = 0.2
+    # learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+    # max_iters = 5000
+    # lr_decay_iters = 5000 # make equal to max_iters usually
+    # min_lr = 1e-4 # learning_rate / 10 usually
+    # beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+    # warmup_iters = 100 # not super necessary potentially
+
+    def elapsed(self, label=""):
+        elapsed_time = time.time() - self.start_time
+        delta_time = elapsed_time - self.last_time if self.last_time > 0.0 else 0.0
+        print(f"{elapsed_time:10.6f}s {delta_time:10.6f}s |\t{label}")
+        self.last_time = elapsed_time
+
+class DataHandler:
+    class Downloader:
+        def __init__(self, download_config: DownloadConfig):
+            self.download_config = download_config
+            self.download_config.download_path.mkdir(parents=True, exist_ok=True)
+        
+        def download(self):
+            for fname, url in self.download_config.get_items():
+                download_text = self.download_url(url, fname)
+                if download_text is None:
+                    continue
+                self.write_file(fname=fname, contents=download_text)
+
+        def write_file(self,fname:str,contents:str):
+            (self.download_config.download_path / fname).write_text(contents, encoding="utf-8")
+        
+        def file_found(self, fname:str):
+            return (self.download_config.download_path / fname).is_file()
+
+        def download_url(self, url: str, fname: str) -> str | None:
+            if self.file_found(fname=fname):
+                print(f"Skipping {url}")
+                return None
+            try:
+                print(f"downloading {url}")
+                response = requests.get(url)
+                return response.text if response.status_code == 200 else None
+            except requests.exceptions.RequestException:
+                return None
+    class Tokenizer:
+        def __init__(self, config: Config):
+            self.config = config
+            self.nlp = spacy.load("en_core_web_sm")
+
+        def save_tokenized(self):
+            self.tokenize_dir(src_dir=self.config.formatted_txt_path,dest_dir=self.config.tokenized_txt_path)
+
+        def tokenize_dir(self, src_dir: Path, dest_dir: Path):
+            src_files = src_dir.glob('*')
+            dest_files = dest_dir.glob('*')
+            dest_dir.mkdir(parents=True, exist_ok=True)
+
+            for src_file in src_files:
+                if not src_file.is_file():
+                    continue
+                dest_file = dest_dir / src_file.name
+                if not dest_file.exists():
+                    self.tokenize_text(src_file, dest_file)
+
+        def tokenize_text(self, src_file: Path, dest_file: Path):
+            print(src_file)
+            text = src_file.read_text(encoding="utf-8")
+            text = text[:1000]
+            doc = self.nlp(text)
+            doc.to_disk(path=dest_file)
+
+
+
+
+
+# class Modules:
+#     class SelfAttention(torch.nn.Module):
+#         def __init__(self, emb, heads):
+#             super().__init__()
+#             assert emb % heads == 0
+#             self.emb, self.heads = emb, heads
+#             self.to_queries = torch.nn.Linear(emb, emb)
+#             self.to_keys = torch.nn.Linear(emb, emb)
+#             self.to_values = torch.nn.Linear(emb, emb)
+#             self.unify = torch.nn.Linear(emb, emb)
+
+#         def forward(self, x):
+#             b, t, emb = x.shape
+#             h = self.heads
+#             queries = self.to_queries(x)
+#             keys = self.to_keys(x)
+#             values = self.to_values(x)
+#             queries = queries.view(b, t, h, emb//h)
+#             keys = keys.view(b, t, h, emb//h)
+#             values = values.view(b, t, h, emb//h)
+#             queries = queries.transpose(1, 2).reshape(b*h, t, emb//h)
+#             keys = keys.transpose(1, 2).reshape(b*h, t, emb//h)
+#             values = values.transpose(1, 2).reshape(b*h, t, emb//h)
+#             W = torch.bmm(queries, keys.transpose(1,2))
+#             W = W / (emb**(1/2))
+#             W = F.softmax(W, dim=2)
+#             y = torch.bmm(W, values).view(b, h, t, emb//h)
+#             y = y.transpose(1, 2).reshape(b, t, emb)
+#             return self.unify(y), W
+
+#     class TransformerBlock(torch.nn.Module):
+#         def __init__(self, config: Config):
+#             super().__init__()
+#             self.attention = Modules.SelfAttention(config.emb, config.heads)
+#             self.norm1 = torch.nn.LayerNorm(config.emb)
+#             self.norm2 = torch.nn.LayerNorm(config.emb)
+#             self.fcn = torch.nn.Sequential(
+#                 torch.nn.Linear(config.emb, 4*config.emb),
+#                 torch.nn.ReLU(),
+#                 torch.nn.Linear(4*config.emb, config.emb)
+#             )
+
+#         def forward(self, x):
+#             attented, W = self.attention(x)
+#             x = self.norm1(attented + x)
+#             ff = self.fcn(x)
+#             return self.norm2(ff + x), W
+
+#     class Transformer(torch.nn.Module):
+#         def __init__(self, config: Config, vocab_size: int):
+#             super().__init__()
+#             self.config = config
+#             self.token_embedding = torch.nn.Embedding(embedding_dim=self.config.emb, num_embeddings=vocab_size)
+#             self.pos_embedding = torch.nn.Embedding(embedding_dim=self.config.emb, num_embeddings=self.config.max_length)
+#             self.tblock = Modules.TransformerBlock(config=self.config)
+#             self.toprobs = torch.nn.Linear(self.config.emb, 2)
+
+#         def forward(self, x):
+#             tokens = self.token_embedding(x)
+#             b, t, e = tokens.shape
+#             positions = self.pos_embedding(torch.arange(t, device="cuda"))[None, :, :].expand(b, t, e)
+#             x = tokens + positions
+#             x, W = self.tblock(x)
+#             x = torch.mean(x, dim=1)
+#             x = self.toprobs(x)
+#             return F.log_softmax(x, dim=1), W
+
+# class DataHandler:
+#     def __init__(self, config: Config):
+#         self.config = config
+#         self.config.elapsed("DataHandler Start")
+#         self.dataset = datasets.load_dataset("zapsdcn/imdb", cache_dir="../data/classification/")
+#         self.config.elapsed("DataHandler load_dataset")
+#         self.train_dataset = self.dataset['train'] 
+#         self.validation_dataset = self.dataset['validation']
+#         self.test_dataset = self.dataset['test']
+#         self.tokenizer = torchtext.data.utils.get_tokenizer("basic_english")
+#         self.config.elapsed("DataHandler get_tokenizer")
+#         self.trim = slice(None,config.max_length,None)
+#         self.tokenize_all()
+#         self.config.elapsed("DataHandler tokenize_all")
+#         self.set_vocab()
+#         self.config.elapsed("DataHandler set_vocab")
+#         self.numericalize_all()
+#         self.config.elapsed("DataHandler numericalize_all")
+#         self.format_all()
+#         self.config.elapsed("DataHandler format_all")
+#         self.set_data_loaders()
+#         self.config.elapsed("DataHandler set_data_loaders")
+
+#     def tokenize(self, data):
+#         tokens = self.tokenizer(data["text"])[self.trim]
+#         return {"tokens": tokens}
+    
+#     def numericalize(self, data):
+#         id = self.vocab.lookup_indices(data["tokens"])
+#         return {"id": id}
+
+#     def tokenize_all(self):
+#         self.train_dataset = self.train_dataset.map(self.tokenize)
+#         self.validation_dataset = self.validation_dataset.map(self.tokenize)
+#         self.test_dataset = self.test_dataset.map(self.tokenize)
+
+#     def numericalize_all(self):
+#         self.train_dataset = self.train_dataset.map(self.numericalize)
+#         self.validation_dataset = self.validation_dataset.map(self.numericalize)
+#         self.test_dataset = self.test_dataset.map(self.numericalize)
+
+#     def format_all(self):
+#         self.train_dataset = self.train_dataset.with_format(type="torch", columns=["id", "label"])
+#         self.validation_dataset = self.validation_dataset.with_format(type="torch", columns=["id", "label"])
+#         self.test_dataset = self.test_dataset.with_format(type="torch", columns=["id", "label"])
+        
+#     def set_vocab(self):
+#         self.vocab = torchtext.vocab.build_vocab_from_iterator(
+#             self.train_dataset["tokens"],
+#             min_freq=self.config.min_freq,
+#             specials=self.config.special_tokens,
+#         )
+#         self.vocab.set_default_index(self.vocab[self.config.default_token])
+
+#     @staticmethod
+#     def get_data_loader(dataset, batch_size, pad_index, shuffle=False):
+#         def get_collate_fn(pad_index):
+#             def collate_fn(batch):
+#                 batch_ids = [i["id"] for i in batch]
+#                 batch_ids = torch.nn.utils.rnn.pad_sequence(
+#                     batch_ids, padding_value=pad_index, batch_first=True
+#                 )
+#                 batch_label = [i["label"] for i in batch]
+#                 batch_label = torch.stack(batch_label)
+#                 batch = {"id": batch_ids, "label": batch_label}
+#                 return batch
+#             return collate_fn
+
+#         collate_fn = get_collate_fn(pad_index)
+#         data_loader = torch.utils.data.DataLoader(
+#             dataset=dataset,
+#             batch_size=batch_size,
+#             collate_fn=collate_fn,
+#             shuffle=shuffle,
+#         )
+#         return data_loader
+
+#     def set_data_loaders(self):
+#         pad_index = self.vocab["<pad>"]
+#         self.train_data_loader = DataHandler.get_data_loader(self.train_dataset, self.config.batch_size, pad_index, shuffle=True)
+#         self.validation_data_loader = DataHandler.get_data_loader(self.validation_dataset, self.config.batch_size, pad_index)
+#         self.test_data_loader = DataHandler.get_data_loader(self.test_dataset, self.config.batch_size, pad_index)
+
+
+
+# class TrainHandler:
+#     def __init__(self, config: Config):
+#         self.config = config
+#         np.random.seed(self.config.seed)
+#         torch.manual_seed(self.config.seed)
+#         torch.cuda.manual_seed(self.config.seed)
+#         torch.backends.cudnn.deterministic = True
+
+#     def load(self, data_handler: DataHandler):
+#         self.config.elapsed("TrainHandler start")
+#         self.data_handler = data_handler
+#         self.model = Modules.Transformer(config=self.config, vocab_size=len(self.data_handler.vocab)).to("cuda")
+#         self.opt = torch.optim.Adam(lr=self.config.lr, params=self.model.parameters())
+#         self.config.elapsed("TrainHandler load")
+
+#     def train(self):
+#         self.config.elapsed("train start")
+#         self.accs = []
+#         for epoch in range(self.config.num_epochs):
+#             for i, batch in enumerate(self.data_handler.train_data_loader):
+#                 self.opt.zero_grad()
+#                 loss = F.nll_loss(self.model(batch["id"].to("cuda"))[0], batch["label"].to("cuda"))
+#                 loss.backward()
+#                 self.opt.step()
+#             with torch.no_grad():
+#                 tot, cor= 0.0, 0.0
+#                 for batch in self.data_handler.validation_data_loader:
+#                     input = batch["id"].to("cuda")
+#                     if input.shape[1] > self.config.max_length:
+#                         input = input[:, :self.config.max_length]
+#                     preds, _ = self.model(input)
+#                     preds = preds.argmax(dim=1)
+#                     tot += float(input.size(0))
+#                     cor += float((batch["label"].to("cuda") == preds).sum().item())
+#                 acc = cor / tot
+#                 self.accs.append(acc)
+#             print("Epoch:{}; Loss: {}; Validation Accuracy: {}".format(epoch, loss.item(), acc))
+
+#     def train_orig(self):
+#         self.config.elapsed("train start")
+#         self.accs = []
+#         for epoch in range(self.config.num_epochs):
+#             for batch in self.data_handler.train_data_loader:
+#                 # self.config.elapsed(f"train batch start")
+#                 self.opt.zero_grad()
+#                 input = batch["id"].to("cuda")
+#                 output = batch["label"].to("cuda")
+#                 preds, _ = self.model(input)
+#                 # self.config.elapsed("train batch pred")
+#                 loss = F.nll_loss(preds, output)
+#                 loss.backward()
+#                 self.opt.step()
+#                 # self.config.elapsed("train batch step")
+#                 with torch.no_grad():
+#                     tot, cor= 0.0, 0.0
+#                     for batch in self.data_handler.validation_data_loader:
+#                         input = batch["id"].to("cuda")
+#                         output = batch["label"].to("cuda")
+#                         if input.shape[1] > self.config.max_length:
+#                             input = input[:, :self.config.max_length]
+#                         preds, _ = self.model(input)
+#                         # self.config.elapsed("train validate pred")
+#                         preds = preds.argmax(dim=1)
+#                         tot += float(input.size(0))
+#                         cor += float((output == preds).sum().item())
+#                         # self.config.elapsed("train validate done")
+#                     acc = cor / tot
+#                     self.accs.append(acc)
+#             print("Epoch:{}; Loss: {}; Validation Accuracy: {}".format(epoch, loss.item(), acc))
+
+#     def save(self):
+#         torch.save(self.model.state_dict(), "trained_models/clasify_{}heads.pt".format(self.config.heads))
+#         np.save("trained_models/acc.npy", self.accs)
+#         self.config.elapsed("save")
+
+# def main():
+#     config=Config()
+#     train_handler = TrainHandler(config=config)
+#     data_handler = DataHandler(config=config)
+#     train_handler.load(data_handler=data_handler)
+#     train_handler.train()
+#     train_handler.save()
+
+# if __name__ == "__main__":
+#     main()
